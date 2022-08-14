e400856a8b2f1dad9d0ba8d7ef087603d29ed84a
jemin <rudanijemin18@gmail.com>
2022-08-14 22:06:23 +0530

    Fetching API Key from News API

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,22 @@ import NewsItem from './NewsItem'
 export default class News extends Component {
   render() {
     return (
-      <div>
-          this is a news
-        <NewsItem/>
+      <div className="container my-3">
+        <h2>News Headlines</h2>
+        <div className="row">
+        {/* col md-3 means that medium divice */}
+          <div className="col md-4">  
+          <NewsItem title="my title" discription ="dsc"/>
+          </div>
+          <div className="col md-4">
+          <NewsItem title="my title" discription ="dsc"/>
+          </div>
+          <div className="col md-4">
+          <NewsItem title="my title" discription ="dsc"/>
+          </div>
+        </div>
+          
+       
 
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title,discription}  = this.props;
+
     return (
       <div>
-        this is a NewsItem
-      </div>
+        <div className="card" style={{width: "18rem"}}>
+          <img src="..." className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{discription}</p>
+            <a href="/" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>  
     )
   }
 }
