10490a73fbce7b348af8b49e3b2a69981009ef8d
jemin <rudanijemin18@gmail.com>
2022-08-16 22:02:13 +0530

    l-27 use fatch API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,8 @@ export class NewsItem extends Component {
     return (
       <div className="container my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <img src={!imageUrl?"https://c.ndtvimg.com/indian-flag-tricolour-generic-pixabay_625x300_1529830758454.jpg":imageUrl} className="card-img-top" alt="..."/>
+          {/* koi image n hoi to  */}
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{discription}</p>
